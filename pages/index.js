cb936640bfb2c278dedbaed657efb3603bcf8712
Cody Robinson <crobinson@skillpath.com>
2023-05-26 20:01:40 -0500

    add tailwind and remove cypress

--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,24 @@ import FeedbackForm from "@components/FeedbackForm";
 import JokeBlock from "@components/JokeBlock";
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Toolbox</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>Next.js Toolbox</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <Header title="Next.js Toolbox" />
-        <hr />
-        <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
-        </p>
-        <FeedbackForm />
-        <JokeBlock />
-      </main>
-      <Footer />
-    </div>
-  );
+            <main>
+                <Header title="Next.js Toolbox" />
+                <hr />
+                <p className="">
+                    Here's an example of a Netlify Form! When you fill this out, the
+                    submissions can be found in the Netlify Admin site.
+                </p>
+                <FeedbackForm />
+                <JokeBlock />
+            </main>
+            <Footer />
+        </div>
+    );
 }
