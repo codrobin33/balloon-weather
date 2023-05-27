e581ca7bd31b92e5d63de8a5fc6bc745fc34ca26
Cody Robinson <crobinson@skillpath.com>
2023-05-27 09:57:44 -0500

    no autocomplete

--- components/Layout.js
+++ components/Layout.js
@@ -169,15 +169,16 @@ export default function Layout() {
                                         Search
                                     </label>
                                     <MagnifyingGlassIcon
-                                        className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+                                        className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-700"
                                         aria-hidden="true"
                                     />
                                     <input
                                         id="search-field"
-                                        className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+                                        className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-700 focus:ring-0 sm:text-sm"
                                         placeholder="Search..."
                                         type="search"
                                         name="search"
+                                        autoComplete="off"
                                     />
                                 </form>
                                 <div className="flex items-center gap-x-4">
